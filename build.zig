ff0af34c4afab5054d81a3cfbadafc15f0863856
CoolPuppyKid <68360803+CoolPuppyKid@users.noreply.github.com>
2026-01-15 02:19:26 -0600

    Fix stbi

--- build.zig
+++ build.zig
@@ -20,15 +20,6 @@ pub fn build(b: *std.Build) void {
     const test_step = b.step("test", "Run tests (NO ITS NOT SHUTTING DOWN!!!)");
 
     const tests = b.addTest(.{ .name = "zig-stbi-tests", .root_module = zigstbi });
-    tests.linkLibC();
-    tests.addIncludePath(b.path("include"));
-    tests.addCSourceFile(.{
-        .file = b.path("include/stb_image.c"),
-        .flags = &.{
-            "-std=c99",
-        },
-        .language = .c,
-    });
 
     b.installArtifact(tests);
     test_step.dependOn(&b.addRunArtifact(tests).step);
